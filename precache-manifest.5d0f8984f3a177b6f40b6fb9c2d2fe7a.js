522e4846b82708e60536ee87e9ca897a36ebb409
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-05-23 15:04:26 +0530

    Updates

---
+++ precache-manifest.5d0f8984f3a177b6f40b6fb9c2d2fe7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d88aeb5ee3426a3207733eeb4e3c04d",
+    "url": "/MonsterSearch/index.html"
+  },
+  {
+    "revision": "235e870e1669c6a22f38",
+    "url": "/MonsterSearch/static/css/main.8207b2b6.chunk.css"
+  },
+  {
+    "revision": "faa3bbc9c83790eab95d",
+    "url": "/MonsterSearch/static/js/2.d23d7c0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonsterSearch/static/js/2.d23d7c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235e870e1669c6a22f38",
+    "url": "/MonsterSearch/static/js/main.ce43b348.chunk.js"
+  },
+  {
+    "revision": "1e9550388d18ea9772c3",
+    "url": "/MonsterSearch/static/js/runtime-main.a4beb666.js"
+  }
+]);
